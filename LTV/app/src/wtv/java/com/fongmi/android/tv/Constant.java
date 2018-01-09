cff32d31fbab7a792bf8139cf9dbc738cc92809c
FongMi <b8892778@gmail.com>
2018-01-09 18:38:59 +0800

    [wtv] initialize

---
+++ LTV/app/src/wtv/java/com/fongmi/android/tv/Constant.java
@@ -0,0 +1,16 @@
+package com.fongmi.android.tv;
+
+class Constant {
+
+    static final String URL = "http://132.148.138.213/wowza/wtv_service.asmx";
+    static final String FREE_GEO = "http://freegeoip.net/json/";
+    static final String TEMP_URI = "http://tempuri.org/";
+    static final String REGISTER_ID = "REGISTER_ID";
+    static final String REGISTER_IP = "REGISTER_IP";
+    static final String REGISTER_MAC = "REGISTER_MAC";
+    static final String WTV_NOTICE = "WTV_NOTICE";
+    static final String WTV_CHANNEL = "WTV_CHANNEL_WOWZA";
+    static final String USER_MAC = "02:00:00:00:00:00";
+    static final String USER_ID = "'owo'";
+
+}
