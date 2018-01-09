436d093666ac115f658066554a4a693bdd25f0dc
FongMi <b8892778@gmail.com>
2018-01-09 16:35:22 +0800

    [all] remove custom toast

--- LTV/app/src/main/java/com/fongmi/android/tv/utils/Notify.java
+++ LTV/app/src/main/java/com/fongmi/android/tv/utils/Notify.java
@@ -1,16 +1,10 @@
 package com.fongmi.android.tv.utils;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fongmi.android.tv.App;
-import com.fongmi.android.tv.R;
 
 public class Notify {
-
-    private Toast mToast;
 
     private static class Loader {
         static volatile Notify INSTANCE = new Notify();
@@ -30,18 +24,6 @@ public class Notify {
 
     private void makeText(String message) {
         if (message.length() < 3) return;
-        if (mToast != null) mToast.cancel();
-        mToast = new Toast(App.getInstance());
-        mToast.setDuration(Toast.LENGTH_LONG);
-        mToast.setView(getLayout(message));
-        mToast.show();
-    }
-
-    private View getLayout(String message) {
-        View layout = LayoutInflater.from(App.getInstance()).inflate(R.layout.view_toast, null, false);
-        TextView view = layout.findViewById(R.id.text);
-        layout.setAlpha(0.9f);
-        view.setText(message);
-        return layout;
+        Toast.makeText(App.getInstance(), message, Toast.LENGTH_LONG).show();
     }
 }
