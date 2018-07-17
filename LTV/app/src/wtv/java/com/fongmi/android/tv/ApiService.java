f10e57cb7338ddfe3a1191c2e7fc44c0b50f9ad7
FongMi <b8892778@gmail.com>
2018-07-17 09:32:01 +0800

    [ltv] update

--- LTV/app/src/wtv/java/com/fongmi/android/tv/ApiService.java
+++ LTV/app/src/wtv/java/com/fongmi/android/tv/ApiService.java
@@ -5,15 +5,7 @@ import com.fongmi.android.tv.network.AsyncCallback;
 import com.fongmi.android.tv.network.BaseApiService;
 import com.fongmi.android.tv.utils.Utils;
 
-import static com.fongmi.android.library.ltv.Constant.LTV_CHANNEL_URL;
-import static com.fongmi.android.library.ltv.Constant.LTV_SAMPLE;
-
 public class ApiService extends BaseApiService {
-
-	@Override
-	public void onInit(AsyncCallback callback) {
-		new WebService(LTV_SAMPLE, callback).executeOnExecutor(mExecutor);
-	}
 
 	@Override
 	public void getChannels(AsyncCallback callback) {
@@ -22,11 +14,11 @@ public class ApiService extends BaseApiService {
 
 	@Override
 	public void getChannelUrl(Channel channel, AsyncCallback callback) {
-		new WebService(LTV_CHANNEL_URL, callback).executeOnExecutor(mExecutor, channel.getUrl());
+		new WebService(callback).executeOnExecutor(mExecutor, channel.getUrl());
 	}
 
 	@Override
 	public void onRetry(AsyncCallback callback) {
-		new WebService(LTV_SAMPLE, callback).executeOnExecutor(mExecutor);
+		callback.onResponse();
 	}
 }
--- LTV/app/src/wtv/java/com/fongmi/android/tv/WebService.java
+++ LTV/app/src/wtv/java/com/fongmi/android/tv/WebService.java
@@ -5,37 +5,21 @@ import android.os.AsyncTask;
 import com.fongmi.android.library.ltv.Ltv;
 import com.fongmi.android.tv.network.AsyncCallback;
 
-import static com.fongmi.android.library.ltv.Constant.LTV_SAMPLE;
-
 class WebService extends AsyncTask<String, Integer, String> {
 
 	private AsyncCallback callback;
-	private String action;
 
-	WebService(String action, AsyncCallback callback) {
-		this.action = action;
+	WebService(AsyncCallback callback) {
 		this.callback = callback;
 	}
 
 	@Override
 	protected String doInBackground(String... params) {
-		switch (action) {
-			case LTV_SAMPLE:
-				return Ltv.getInstance().getSample();
-			default:
-				return Ltv.getInstance().getUrl(params[0]);
-		}
+		return Ltv.getInstance().getUrl(params[0]);
 	}
 
 	@Override
 	protected void onPostExecute(String result) {
-		switch (action) {
-			case LTV_SAMPLE:
-				callback.onResponse();
-				break;
-			default:
-				callback.onResponse(result);
-				break;
-		}
+		callback.onResponse(result);
 	}
 }
