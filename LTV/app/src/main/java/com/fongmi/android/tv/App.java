04cbe8ec6d47f8f07f68ab3f5539a4c0834a0b37
FongMi <b8892778@gmail.com>
2019-04-17 10:13:35 +0800

    [all] add custom user-agent

--- LTV/app/src/main/java/com/fongmi/android/tv/App.java
+++ LTV/app/src/main/java/com/fongmi/android/tv/App.java
@@ -1,6 +1,15 @@
 package com.fongmi.android.tv;
 
 import android.app.Application;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.devbrackets.android.exomedia.ExoMedia;
+import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.TransferListener;
+
+import okhttp3.OkHttpClient;
 
 public class App extends Application {
 
@@ -12,5 +21,21 @@ public class App extends Application {
 
 	public static App getInstance() {
 		return instance;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		configureExoMedia();
+	}
+
+	private void configureExoMedia() {
+		ExoMedia.setDataSourceFactoryProvider(new ExoMedia.DataSourceFactoryProvider() {
+			@NonNull
+			@Override
+			public DataSource.Factory provide(@NonNull String userAgent, @Nullable TransferListener listener) {
+				return new OkHttpDataSourceFactory(new OkHttpClient(), "VasCreativePlayer/02.12.0183 (Linux;Android 5.1.1) ExoPlayerLib/2.0.0", listener);
+			}
+		});
 	}
 }
