82b8661c98b1b144d6928fc0724b2a1fce7f2bbd
FongMi <b8892778@gmail.com>
2020-03-06 10:15:43 +0800

    [app] tvbus support x86

--- LTV/app/src/main/java/com/fongmi/android/ltv/utils/Utils.java
+++ LTV/app/src/main/java/com/fongmi/android/ltv/utils/Utils.java
@@ -3,7 +3,6 @@ package com.fongmi.android.ltv.utils;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
-import android.os.Build;
 import android.util.DisplayMetrics;
 import android.view.KeyEvent;
 import android.view.View;
@@ -95,8 +94,7 @@ public class Utils {
 	}
 
 	public static void setImmersiveMode(Activity activity) {
-		int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) flags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+		int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 		activity.getWindow().getDecorView().setSystemUiVisibility(flags);
 	}
 }
