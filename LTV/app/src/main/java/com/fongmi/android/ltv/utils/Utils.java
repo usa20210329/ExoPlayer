f5466d9d61937e0d844d0d99b906d2d997ffc1ed
FongMi <b8892778@gmail.com>
2021-01-12 10:08:32 +0800

    [app] fix pip crash on some device

--- LTV/app/src/main/java/com/fongmi/android/ltv/utils/Utils.java
+++ LTV/app/src/main/java/com/fongmi/android/ltv/utils/Utils.java
@@ -4,7 +4,6 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.app.PictureInPictureParams;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.provider.Settings;
 import android.util.DisplayMetrics;
@@ -108,10 +107,14 @@ public class Utils {
 	}
 
 	public static void enterPIP(Activity activity) {
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O || !App.get().getPackageManager().hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE) || activity.isInPictureInPictureMode()) return;
-		PictureInPictureParams.Builder builder = new PictureInPictureParams.Builder();
-		builder.setAspectRatio(new Rational(16, 9)).build();
-		activity.enterPictureInPictureMode(builder.build());
+		try {
+			if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O || activity.isInPictureInPictureMode()) return;
+			PictureInPictureParams.Builder builder = new PictureInPictureParams.Builder();
+			builder.setAspectRatio(new Rational(16, 9)).build();
+			activity.enterPictureInPictureMode(builder.build());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	static String getAndroidId() {
