bee509a2b76465bd4a147c169260c521611e0565
FongMi <b8892778@gmail.com>
2021-01-20 17:59:38 +0800

    [app] fix tvbus problem

--- LTV/app/src/main/java/com/fongmi/android/ltv/App.java
+++ LTV/app/src/main/java/com/fongmi/android/ltv/App.java
@@ -1,8 +1,10 @@
 package com.fongmi.android.ltv;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.fongmi.android.ltv.utils.Utils;
+import com.tvbus.engine.PmsHook;
 
 public class App extends Application {
 
@@ -18,5 +20,11 @@ public class App extends Application {
 
 	public static String getName() {
 		return Utils.getString(R.string.app_name).toLowerCase();
+	}
+
+	@Override
+	protected void attachBaseContext(Context base) {
+		super.attachBaseContext(base);
+		PmsHook.inject(base);
 	}
 }
---
+++ LTV/tvbus/src/main/java/com/tvbus/engine/PmsHook.java
@@ -0,0 +1,77 @@
+package com.tvbus.engine;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class PmsHook implements InvocationHandler {
+
+	private static final int GET_SIGNATURES = 0x00000040;
+
+	private String appPkgName;
+	private byte[][] sign;
+	private Object base;
+
+	public static void inject(Context context) {
+		new PmsHook().hook(context);
+	}
+
+	public void hook(Context context) {
+		try {
+			Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+			Class<?> iPackageManagerInterface = Class.forName("android.content.pm.IPackageManager");
+			Method currentActivityThreadMethod = activityThreadClass.getDeclaredMethod("currentActivityThread");
+			Object currentActivityThread = currentActivityThreadMethod.invoke(null);
+			Field sPackageManagerField = activityThreadClass.getDeclaredField("sPackageManager");
+			sPackageManagerField.setAccessible(true);
+			this.sign = getSign(context);
+			this.base = sPackageManagerField.get(currentActivityThread);
+			this.appPkgName = context.getPackageName();
+			Object proxy = Proxy.newProxyInstance(iPackageManagerInterface.getClassLoader(), new Class<?>[]{iPackageManagerInterface}, this);
+			sPackageManagerField.set(currentActivityThread, proxy);
+			PackageManager pm = context.getPackageManager();
+			Field mPmField = pm.getClass().getDeclaredField("mPM");
+			mPmField.setAccessible(true);
+			mPmField.set(pm, proxy);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private byte[][] getSign(Context context) throws IOException {
+		DataInputStream is = new DataInputStream(new ByteArrayInputStream(Base64.decode(context.getString(R.string.data), Base64.DEFAULT)));
+		byte[][] sign = new byte[is.read() & 0xFF][];
+		for (int i = 0; i < sign.length; i++) {
+			sign[i] = new byte[is.readInt()];
+			is.readFully(sign[i]);
+		}
+		return sign;
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		if (method.getName().equals("getPackageInfo")) {
+			String pkgName = (String) args[0];
+			Integer flag = (Integer) args[1];
+			if ((flag & GET_SIGNATURES) != 0 && appPkgName.equals(pkgName)) {
+				PackageInfo info = (PackageInfo) method.invoke(base, args);
+				info.signatures = new Signature[this.sign.length];
+				for (int i = 0; i < info.signatures.length; i++) {
+					info.signatures[i] = new Signature(this.sign[i]);
+				}
+				return info;
+			}
+		}
+		return method.invoke(base, args);
+	}
+}
