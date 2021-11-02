1100a78637584785674bafe6af854b5296a5fe1d
FongMi <b8892778@gmail.com>
2021-11-02 15:08:30 +0800

    [app] support app check

--- LTV/app/src/main/java/com/fongmi/android/ltv/App.java
+++ LTV/app/src/main/java/com/fongmi/android/ltv/App.java
@@ -3,6 +3,9 @@ package com.fongmi.android.ltv;
 import android.app.Application;
 import android.content.Context;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory;
 import com.tvbus.engine.PmsHook;
 
 public class App extends Application {
@@ -21,5 +24,12 @@ public class App extends Application {
 	protected void attachBaseContext(Context base) {
 		PmsHook.inject(base);
 		super.attachBaseContext(base);
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		FirebaseApp.initializeApp(this);
+		FirebaseAppCheck.getInstance().installAppCheckProviderFactory(SafetyNetAppCheckProviderFactory.getInstance());
 	}
 }
