300c0acc6f16875928914c9574286b6aba3abc41
FongMi <b8892778@gmail.com>
2021-12-20 09:04:51 +0800

    [app] update

--- Main/app/src/main/java/com/fongmi/android/tv/App.java
+++ Main/app/src/main/java/com/fongmi/android/tv/App.java
@@ -3,9 +3,6 @@ package com.fongmi.android.tv;
 import android.app.Application;
 import android.content.Context;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.appcheck.FirebaseAppCheck;
-import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory;
 import com.tvbus.engine.PmsHook;
 
 public class App extends Application {
@@ -24,12 +21,5 @@ public class App extends Application {
 	protected void attachBaseContext(Context base) {
 		PmsHook.inject(base);
 		super.attachBaseContext(base);
-	}
-
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		FirebaseApp.initializeApp(this);
-		FirebaseAppCheck.getInstance().installAppCheckProviderFactory(SafetyNetAppCheckProviderFactory.getInstance());
 	}
 }
